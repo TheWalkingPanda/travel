133fc459d0b6191812644432ec19c62467058c53
TheWalkingPanda <2770078682@qq.com>
2015-06-21 12:47:15 +0800

    TravelStaticValue.java add data_state_U & E

--- src/main/java/com/travel/util/TravelStaticValue.java
+++ src/main/java/com/travel/util/TravelStaticValue.java
@@ -1,5 +1,7 @@
 package com.travel.util;
 
 public class TravelStaticValue {
-	public static final String TEST_VALUE = "test";
+	//数据有效性
+	public static final String DATA_STATE_U = "U";//有效
+	public static final String DATA_STATE_E = "E";//失效
 }
