7cf567b0d8fcdb01130957dfd43a48313a423052
bingbinglao <bingbinglao@hengtiansoft.com>
2015-06-23 10:43:04 +0800

    add memcached log

---
+++ src/main/java/com/travel/advice/MemcachedLogAdvice.java
@@ -0,0 +1,33 @@
+package com.travel.advice;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+import com.travel.util.TravelStaticValue;
+
+@Component
+@Aspect
+public class MemcachedLogAdvice {
+	private static Log log = LogFactory.getLog(MemcachedLogAdvice.class);
+	
+	
+	@Around("execution(* com.danga.MemCached.MemCachedClient.*(..))")
+	public void memcachedLog(ProceedingJoinPoint pjp) throws Throwable{
+		String methodName = pjp.getSignature().getName();
+		Object[] args = pjp.getArgs();
+		String logArgs = "";
+		if(args!=null && 0<args.length){
+			for(Object arg : args){
+				logArgs += arg+",";
+			}
+		}
+		Object ret = pjp.proceed();
+		log.info(TravelStaticValue.LOG_PREFIX + "Memcached >>> method:["+methodName+"] -- "
+															   +"args:["+logArgs+"] -- "
+															 +"return:["+ret+"]");
+	}
+}
--- src/main/java/com/travel/util/TravelStaticValue.java
+++ src/main/java/com/travel/util/TravelStaticValue.java
@@ -4,4 +4,6 @@ public class TravelStaticValue {
 	//数据有效性
 	public static final String DATA_STATE_U = "U";//有效
 	public static final String DATA_STATE_E = "E";//失效
+	
+	public static final String LOG_PREFIX = ">>>>> ";//打印日志前缀
 }
